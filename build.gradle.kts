4c91a6250620413e23802c446c4624067b67d105
Joeys <ky6115@163.com>
2021-05-24 17:44:33 +0800

    kotlin upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha15")
+        classpath("com.android.tools.build:gradle:7.0.0-beta02")
         classpath(com.joeys.wanAndroid.buildsrc.Libs.Kotlin.gradlePlugin)
     }
 }
--- buildSrc/src/main/java/com/joeys/wanAndroid/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/joeys/wanAndroid/buildsrc/dependencies.kt
@@ -21,14 +21,14 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.4.32"
+        private const val version = "1.5.0"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
     }
 
     object Coroutines {
-        private const val version = "1.4.2"
+        private const val version = "1.4.3"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
         const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
         const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
